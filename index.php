baab3df38f657991281a7990c09dda199d54a15d
RvSpijker <r.vanspijker@student.alfa-college.nl>
2023-06-21 12:20:18 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <h1 class="title">Naam Blog</h1>
+    </header>
+    <main class="grid-container">
+        <div class="item1 mainblog">
+            <h1 class="blogtitle">blogtitle</h1>
+            <h2 class="blogintro">blogintroblogintroblogintroblogintroblogintroblogintroblogintro</h2>
+            <img class="blogimg" src="" alt="">
+            <p class="blogtxt">a</p>
+        </div>
+    </main>
+    <div class="item2 secblog">
+        <img src="" alt="">
+        <h1>title</h1>
+    </div>
+    <div class="item3 secblog">
+        <img src="" alt="">
+        <h1>title</h1>
+    </div>
+    <h1 class="item4">See More</h1>
+</body>
+</html>
